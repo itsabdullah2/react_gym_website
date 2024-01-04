7a82425b722f86b480c253190a9e516b9a91b9d2
itsabdullah2 <abdullahosman3020@gmail.com>
2024-01-04 17:10:39 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
